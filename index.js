d725fa04eca3c53521dbaf66f9f051660f83788f
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-08-14 09:13:08 +0545

    learn about vars

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //Comment synta
 
+
 /*
 multi line comment
 super 
 long
 comment
 */
+
+var person ="Rahul"; // 5 ,true,
+person ="Hari"; // reassign
+
+const person1 ="Johny"; // fixed value
+let person2 = "Riya"; //changable value
+person2="siya"
+
+console.log({person,person1,person2});
