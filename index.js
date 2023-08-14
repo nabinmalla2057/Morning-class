533478b844bfaf8c2b99ef903c94bd489bbcc628
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-08-14 09:17:38 +0545

    learn about interactions

--- index.js
+++ index.js
@@ -15,4 +15,11 @@ const person1 ="Johny"; // fixed value
 let person2 = "Riya"; //changable value
 person2="siya"
 
-console.log({person,person1,person2});
+// Interactions
+alert("JS class is in session");
+person2 =prompt("What is your name ?");
+const isLegal= confirm("Are u 18 yesrs or above?");
+
+
+console.log({person,person1,person2,isLegal});
+
