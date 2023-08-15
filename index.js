571dae9cc7005e922f7f00ed1a7c18d4c0464e90
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-08-15 08:54:15 +0545

    task given

--- index.js
+++ index.js
@@ -23,3 +23,55 @@ const isLegal= confirm("Are u 18 yesrs or above?");
 
 console.log({person,person1,person2,isLegal});
 
+
+// OPERATORS
+ 
+
+// interactions
+
+
+//Statement
+if (a===0) {
+}  else if (a===1){   
+}  else if (a===2){
+}  else if (a===3)
+
+//Ternory Operator
+if (a===0) {
+    console.log("its 0");
+}   else {
+    console.log("its not 0");
+}
+
+// Switch Case
+
+switch (a===0){
+    case Value
+
+
+
+
+
+}
+
+
+
+
+
+//Task 1
+// prompt user to enter a number
+// check if the number is which day of the week 
+// Example 0=> Sunday , 1=>Monday , 2=>Tuesday,....
+
+
+
+// Task 2
+// prompt user to enter a first number
+// prompt user to enter a second number 
+// prompt user to enter a operator
+// using switch case
+// perform the operation and display the result
+
+
+
+// Task 1
